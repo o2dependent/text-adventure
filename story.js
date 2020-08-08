da78b33b88ab4fcfe9674aae60fa0f3faee96a82
Ethan Olsen <46045126+o2dependent@users.noreply.github.com>
2020-08-07 23:32:57 -0700

    Add files via upload

---
+++ story.js
@@ -0,0 +1,49 @@
+let story = {
+	room1: {
+		name: 'Chest Room',
+		entrance: 'Hello welcome to room 1',
+		options: '1 - Inspect the large chest <br>2 - Enter the door to the left <br>3 - Enter the door to your right',
+		extraOptions: {
+			extra1: '1 - Inspect the alter the chest is on <br>2 - Inspect'
+		}
+	},
+	room2: {
+		name: 'The Butchery',
+		entrance: 'Hello welcome to room 2',
+		options:
+			'1 - Inspect the refrigeration unit in the back of the room <br>2 - Check out the butchers table <br>3 - Inspect the blood covered floor <br>4 - Return to the alter room',
+		extraOptions: {
+
+		}
+	},
+	room3: {
+		name: 'The Hallway',
+		entrance: 'Hello welcome to room 3',
+		options:
+			'1 - Enter the door to the left <br>2 - Enter the door to the right <br>3 - Enter the large steel door <br>4 - Return to the chest room',
+		extraOptions: {
+
+		}
+	},
+	room4: {
+		name: 'The Office',
+		entrance: 'Hello welcome to room 4',
+		options:
+			'1 - Inspect the desk <br>2 - Inspect the cubicles <br>3 - Inspect the whiteboard <br>4 - Exit to the hall',
+		extraOptions: {
+            
+		}
+	},
+	room5: {
+		name: 'The Bathroom',
+		entrance: 'Hello welcome to room 5',
+		options:
+            '1 - Investigate the mirror <br>2 - Investigate the toilet <br>3 - Investigate the shower <br>4 - Exit the bathroom',
+        extraOptions: {
+
+        }
+		extraOptions:{
+			
+		}
+	},
+};
